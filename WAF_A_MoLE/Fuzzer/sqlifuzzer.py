1df32c2d62209f10a66d882a22b44b6db954c39e
Edward Daemon <20520691@gm.uit.edu.vn>
2023-04-18 10:50:53 +0700

    nothing special

--- WAF_A_MoLE/Fuzzer/sqlifuzzer.py
+++ WAF_A_MoLE/Fuzzer/sqlifuzzer.py
@@ -1,11 +1,38 @@
 import random
 import re
 
+def type_check(obj, type, name):
+    if not isinstance(obj, type):
+        raise TypeError(name + ' must be of type ' + str(type))
+    
+def replace_random(candidate, sub, wanted):
+    type_check(candidate, str, "candidate")
+    type_check(sub, str, "sub")
+    type_check(wanted, str, "wanted")
+    occurrences = [m.start() for m in re.finditer(re.escape(sub), candidate)]
+    if not occurrences:
+        return candidate
+
+    pos = random.choice(occurrences)
+
+    before = candidate[:pos]
+    after = candidate[pos:]
+    after = after.replace(sub, wanted, 1)
+
+    result = before + after
+    return result
+
 def random_string(length):
     #generate a random string of length 'length'
     #e.g. random_string(5) -> 'aBcDe'
     dictionary = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     return ''.join(random.choice(dictionary) for i in range(random.randint(1,length)))
+
+def filter_candidates(symbols,payload):
+    type_check(symbols, dict, 'symbols')
+    type_check(payload, str, 'payload')
+
+    return [s for s in symbols.keys() if s in payload]
 
 def case_swapping(payload):
     #randomly swap the case of the payload
@@ -64,6 +91,42 @@ def integer_encoding(payload):
         payload = payload.replace(d, '0x' + hex(int(d))[2:])
     return payload
 
-print(integer_encoding('admin\' or 123=1'))
+def operator_swapping(payload):
+    #change operators to their equivalent
+    #e.g. 'and' -> '&&'
+    symbols = {
+        # OR
+        "||": [" OR ", " || "],
+        " || ": [" OR ", "||"],
+        "OR": [" OR ", "||"],
+        "  OR  ": [" OR ", "||", " || "],
+        # AND
+        "&&": [" AND ", " && "],
+        " && ": ["AND", " AND ", " && "],
+        "AND": [" AND ", "&&", " && "],
+        "  AND  ": [" AND ", "&&"],
+        # Not equals
+        "<>": ["!=", " NOT LIKE "],
+        "!=": [" != ", "<>", " <> ", " NOT LIKE "],
+        # Equals
+        " = ": [" LIKE ", "="],
+        "LIKE": [" LIKE ", "="]
+    }
+    symbols_in_payload = filter_candidates(symbols, payload)
+    if not symbols_in_payload:
+        return payload
+    
+    #randomly choose a symbol to swap
+    symbol_replace = random.choice(symbols_in_payload)
+    #check for possible replacements
+    replacements = symbols[symbol_replace]
+    #randomly choose a replacement
+    replacement = random.choice(replacements)
+    #replace the symbol with the replacement
+    payload = replace_random(payload, symbol_replace, replacement)
+    return payload
+
+
+print(operator_swapping('admin\' and \'1\' = \'1'))
 
     
